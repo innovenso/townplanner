ed682e907640209a1c79c37551f70570c251ee3b
Jurgen Lust <jurgen@innovenso.com>
2022-02-04 18:52:10 +0100

    adding DSL

--- src/test/scala/com/innovenso/townplanner/model/concepts/EnterpriseSpec.scala
+++ src/test/scala/com/innovenso/townplanner/model/concepts/EnterpriseSpec.scala
@@ -10,36 +10,28 @@ import com.innovenso.townplanner.model.meta.{Description, Title}
 import org.scalatest.GivenWhenThen
 import org.scalatest.flatspec.AnyFlatSpec
 
-import scala.language.postfixOps
-
 class EnterpriseSpec extends AnyFlatSpec with GivenWhenThen {
   "Enterprises" can "be added to the townplan" in {
-    val architecture = new TownPlanFactory
+    val ea = new TownPlanFactory
 
     val innovenso =
-      architecture describes Enterprise(title = Title("Innovenso")) as { it =>
+      ea describes Enterprise(title = Title("Innovenso")) as { it =>
         it has Documentation(description = Description(Some("hello")))
       }
-    println(innovenso)
+
     val geniusfish =
-      architecture has Enterprise(title = Title("Genius Fish"))
+      ea has Enterprise(title = Title("Genius Fish"))
 
     val innovensogroup =
-      architecture describes Enterprise(title = Title("Innovenso Group")) as {
-        it =>
-          it triggers geniusfish
+      ea describes Enterprise(title = Title("Innovenso Group")) as { it =>
+        it triggers geniusfish
       }
 
-    architecture hasRelationship Relationship(
+    ea hasRelationship Relationship(
       source = innovenso.key,
       target = geniusfish.key,
       relationshipType = Triggers,
       title = Title("hello world")
     )
-
-    println(geniusfish)
-    println(innovensogroup)
-    architecture.townPlan.relationships.foreach(r => println(r))
-
   }
 }
