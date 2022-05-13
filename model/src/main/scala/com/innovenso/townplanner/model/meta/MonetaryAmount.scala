c9f47bfe276be52c0e91f9eac1c1131316e4f310
Jurgen Lust <jurgen@innovenso.com>
2022-05-13 16:14:10 +0200

    added costing table to web pages

--- model/src/main/scala/com/innovenso/townplanner/model/meta/MonetaryAmount.scala
+++ model/src/main/scala/com/innovenso/townplanner/model/meta/MonetaryAmount.scala
@@ -2,4 +2,9 @@ package com.innovenso.townplanner.model.meta
 
 import java.util.Currency
 
-case class MonetaryAmount(amount: Double, currency: Currency = Currency.getInstance("EUR"))
+case class MonetaryAmount(
+    amount: Double,
+    currency: Currency = Currency.getInstance("EUR")
+) {
+  override def toString: String = f"${currency.getSymbol}${amount}%2.2f"
+}
--- model/src/main/scala/com/innovenso/townplanner/model/meta/UnitCount.scala
+++ model/src/main/scala/com/innovenso/townplanner/model/meta/UnitCount.scala
@@ -1,3 +1,5 @@
 package com.innovenso.townplanner.model.meta
 
-case class UnitCount(count: Double)
+case class UnitCount(count: Double) {
+  override def toString: String = f"${count}%2.2f"
+}
